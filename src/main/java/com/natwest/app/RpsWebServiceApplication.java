13bdcfc0793f0783d91900c5ca63d4b11463f6d3
sahil-pal <sahilpal67@gmail.com>
2022-12-18 09:17:31 +0530

    Spring Project Created

---
+++ src/main/java/com/natwest/app/RpsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natwest/app/RpsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
